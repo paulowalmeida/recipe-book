f6196750571917ac17e0ae13a8b6c0c2b5ec3bdd
Paulo Weskley de A. Ferreira <pauloalmeida@inatel.br>
2021-05-20 22:33:20 -0300

    Building the initial architeture

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TheBasic
+# Recipe Book
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.2.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'the-basic'`, () => {
+  it(`should have as title 'recipe-book'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('the-basic');
+    expect(app.title).toEqual('recipe-book');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('the-basic app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('recipe-book app is running!');
   });
 });
